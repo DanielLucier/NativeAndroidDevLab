5b3e1ac28fa133e6c391c12470334f094bce06c7
DanielLucier <dlucier117@hotmail.com>
2018-02-21 11:55:18 -0600

    display user input as popup

--- Tutorial1_GettingStarted/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ Tutorial1_GettingStarted/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -2,6 +2,9 @@ package com.vogella.android.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateUserActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+    }
+
+    public void onClick(View view) {
+        EditText input = (EditText) findViewById(R.id.username);
+        String string = input.getText().toString();
+        Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
     }
 }
