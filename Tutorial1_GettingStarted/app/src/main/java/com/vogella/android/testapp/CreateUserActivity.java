2644b3492579f286ee8fd4cdc61d201dccbd8c5f
DanielLucier <dlucier117@hotmail.com>
2018-02-21 18:48:27 -0600

    added gender radio buttons

--- Tutorial1_GettingStarted/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
+++ Tutorial1_GettingStarted/app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -4,19 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    boolean male = true;
+    String gender = "male";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+        final RadioGroup radioGroup = findViewById(R.id.gender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged (RadioGroup group,int checkedId){
+                switch (checkedId) {
+                    case R.id.male:
+                        male = true;
+                        break;
+                    case R.id.female:
+                        male = false;
+                        break;
+                }
+            }
+        });
     }
 
     public void onClick(View view) {
-        EditText input = (EditText) findViewById(R.id.username);
+        EditText input = findViewById(R.id.username);
         String string = input.getText().toString();
-        Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
+        if (!male) {
+            gender = "female";
+        }
+
+        Toast.makeText(this, "User " + string + " created. \nGender is " + gender + ".", Toast.LENGTH_LONG).show();
     }
 }
+
