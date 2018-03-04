3ccaf5eb3030d5f15d04583fb80f6409f2384ce3
DanielLucier <dlucier117@hotmail.com>
2018-03-04 15:16:05 -0600

    custom adapter -- performance optimization

--- Tutorial2_ListView/app/src/main/java/android/vogella/de/listactivity/MySimpleArrayAdapter.java
+++ Tutorial2_ListView/app/src/main/java/android/vogella/de/listactivity/MySimpleArrayAdapter.java
@@ -4,7 +4,7 @@ package android.vogella.de.listactivity;
  * Created by Dan on 3/4/2018.
  */
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +13,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MySimpleArrayAdapter extends ArrayAdapter<String> {
-    private final Context context;
+    private final  Activity context;
     private final String[] values;
 
-    public MySimpleArrayAdapter(Context context, String[] values) {
+    static class ViewHolder {
+        public TextView text;
+        public ImageView image;
+    }
+
+    public MySimpleArrayAdapter(Activity context, String[] values) {
         super(context, R.layout.rowlayout, values);
         this.context = context;
         this.values = values;
@@ -24,19 +29,27 @@ public class MySimpleArrayAdapter extends ArrayAdapter<String> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.label);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        textView.setText(values[position]);
-        // Change the icon for Windows and iPhone
+        View rowView = convertView;
+        // reuse views
+        if (rowView == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.rowlayout, null);
+            // configure view holder
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.text = (TextView) rowView.findViewById(R.id.label);
+            viewHolder.image = (ImageView) rowView.findViewById(R.id.icon);
+            rowView.setTag(viewHolder);
+        }
+
+        // fill data
+        ViewHolder holder = (ViewHolder) rowView.getTag();
         String s = values[position];
+        holder.text.setText(s);
         if (s.startsWith("Windows7") || s.startsWith("iPhone")
                 || s.startsWith("Solaris")) {
-            imageView.setImageResource(R.drawable.no);
+            holder.image.setImageResource(R.drawable.no);
         } else {
-            imageView.setImageResource(R.drawable.ok);
+            holder.image.setImageResource(R.drawable.ok);
         }
 
         return rowView;
