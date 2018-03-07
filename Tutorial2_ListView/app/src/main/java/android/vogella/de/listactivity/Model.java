b055bb1111d9531c90e604c25b3f6fa593fa37dc
DanielLucier <dlucier117@hotmail.com>
2018-03-07 16:11:41 -0600

    select items from list example

---
+++ Tutorial2_ListView/app/src/main/java/android/vogella/de/listactivity/Model.java
@@ -0,0 +1,33 @@
+package android.vogella.de.listactivity;
+
+/**
+ * Created by Dan on 3/7/2018.
+ */
+
+public class Model {
+
+    private String name;
+    private boolean selected;
+
+    public Model(String name) {
+        this.name = name;
+        selected = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+}
